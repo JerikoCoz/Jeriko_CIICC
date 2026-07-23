fd3956e4615bc9850aa3032835c9a7e065ab5b81
jeriko3323 <jerikocoz@gmail.com>
2026-07-23 12:16:44 +0800

    task2-Complete

---
+++ src/Task2Chap3.java
@@ -0,0 +1,12 @@
+class Task2{
+    public static void main(String[] args) {
+        char single = 'H';
+        int num3 = 31;
+        short num2 = 10;
+        byte num1 = 0;
+        float point = 2.0f;
+        boolean check = true;
+        String output = single + num3 + num2 + " " + "w" + num1 + "rld" + " " + point + " " + check;
+        System.out.println(output);
+    }
+}
